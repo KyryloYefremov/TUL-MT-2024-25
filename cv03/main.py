40fc0a8d6937b22697b3eb609aaeef724d0a82fb
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-10-05 19:51:08 +0200

    add filtering red color

--- cv03/main.py
+++ cv03/main.py
@@ -1,5 +1,8 @@
+from ctypes.wintypes import tagPOINT
+
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def read_bmp(file):
@@ -63,5 +66,31 @@ def read_bmp(file):
     plt.show()
 
 
+def read_jpg(file):
+    jpg = cv2.imread(file)
+    jpg = cv2.cvtColor(jpg, cv2.COLOR_BGR2RGB)
+
+    white = np.ones(3) * 255  # RGB white color
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    axs[0].imshow(jpg)
+    axs[0].set_title('RGB')
+
+    # Go through all pixels and check if the red color is less than 50%. If so, change the color to white
+    for i in range(jpg.shape[0]):
+        for ii in range(jpg.shape[1]):
+            RGB = jpg[i][ii]
+            r = RGB[0] / np.sum(RGB)
+            if r < 0.5:
+                jpg[i][ii] = white
+
+    axs[1].imshow(jpg)
+    axs[1].set_title('Red only')
+
+    fig.tight_layout()
+    plt.show()
+
+
 if __name__ == '__main__':
     read_bmp("cv03_objekty1.bmp")
+    read_jpg("cv03_red_object.jpg")
